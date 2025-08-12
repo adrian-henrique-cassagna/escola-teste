a7072ce29b3d08c43fb2533314fdd7c61585c4bf
zacajaca21 <zacajaca21@gmail.com>
2025-08-12 11:32:29 -0300

    ajustes de rros na pasta aluno

--- etapa/tests/test_media_aluno.py
+++ etapa/tests/test_media_aluno.py
@@ -2,9 +2,26 @@ import pytest
 from etapa.resultado import calcular_media_do_aluno
 
 
-def teste_com_nota_vaxia():
+def test_com_nota_string():
     nota = ""
 
     with pytest.raises(ValueError, match = "Por favor, digite um valor válido"):
         calcular_media_do_aluno(nota)
- 
+
+def test_com_nota_alta():
+    nota = 9
+
+    with pytest.raises(ValueError, match = "passou"):
+        calcular_media_do_aluno(nota)
+
+def test_com_nota_baixa():
+    nota = 5.7
+
+    with pytest.raises(ValueError, match = "recuperação"):
+        calcular_media_do_aluno(nota)
+
+def test_com_nota_reprovado():
+    nota = 2
+
+    with pytest.raises(ValueError, match = "reprovado"):
+        calcular_media_do_aluno(nota)
